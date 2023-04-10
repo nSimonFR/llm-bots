58ed8cf14aa0a4efd1b278112488ab94ec53d4ca
Nicolas Simon <contact@nsimon.fr>
2023-04-10 22:25:25 +0200

    Add tests for ChatGPT+

---
+++ tests/chagpt+.test.js
@@ -0,0 +1,70 @@
+import dotenv from "dotenv";
+
+import chatGPTPlus from "../src/bots/chagpt+";
+import { generateText } from "../src/bots/chagpt+/commands/text";
+import { generateImage } from "../src/bots/chagpt+/commands/image";
+import { generateAudio } from "../src/bots/chagpt+/commands/audio";
+
+dotenv.config();
+
+const TIMEOUT = 60000;
+const env = process.env;
+
+describe("chatGPT+", () => {
+  test(
+    "text generation message",
+    async () => {
+      const prompt = "Generate a small story about a cat";
+
+      const result = await chatGPTPlus(env, prompt);
+      console.log(result.text);
+
+      expect(result.text).toContain("cat");
+    },
+    TIMEOUT
+  );
+
+  test(
+    "image generation message",
+    async () => {
+      const prompt = "Generate an image of a black cat on a white background";
+
+      const result = await chatGPTPlus(env, prompt);
+
+      expect(result.text).toContain("Generated image");
+    },
+    TIMEOUT
+  );
+
+  test(
+    "audio generation message",
+    async () => {
+      const prompt = "Generate an audio story about a cat";
+
+      const result = await chatGPTPlus(env, prompt);
+
+      expect(result.text).toContain("Generated audio");
+    },
+    TIMEOUT
+  );
+});
+
+describe.only("commands", () => {
+  test("generateText", async () => {
+    await generateText(env, "A blue man walks into a store.");
+    // TODO test telegram sent
+    expect(1).toBe(1);
+  });
+
+  test.only("generateImage", async () => {
+    await generateImage(env, "A robot eating the earth.");
+    // TODO test telegram sent
+    expect(1).toBe(1);
+  });
+
+  test("generateAudio", async () => {
+    await generateAudio(env, "A blue man walks into a store.");
+    // TODO test telegram sent
+    expect(1).toBe(1);
+  });
+});
