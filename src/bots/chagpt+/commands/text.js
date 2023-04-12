7e962daa067b76040d3b7adc1a138b7a9523f332
Nicolas Simon <contact@nsimon.fr>
2023-04-12 21:09:28 +0200

    fixup! Refactor: command and settings, and use enum for audio/image for voice/motor

--- src/bots/chagpt+/commands/text.js
+++ src/bots/chagpt+/commands/text.js
@@ -1,29 +1,12 @@
-import { ChatGPTUnofficialProxyAPI } from "chatgpt";
-
-import { errorAndTimeoutWrapper } from "../../../utils/commons";
-
-export const generateText = async (env, { prompt }) => {
-  // TODO Use separate memory buffer
-  const chatGPTAPI = new ChatGPTUnofficialProxyAPI({
-    accessToken: env.OPENAI_ACCESS_TOKEN,
-    apiReverseProxyUrl: env.OPENAI_PROXY_URL,
-  });
-
-  const result = await errorAndTimeoutWrapper(
-    env,
-    chatGPTAPI.sendMessage(prompt)
-  );
-
-  return result.text;
-};
+export const replyText = async (env, { prompt }) => prompt;
 
 const settings = {
-  command_name: "generate_audio",
+  command_name: "reply_text",
   args: { prompt: "<text>" },
 };
 
 export default {
-  name: "generate_text",
+  name: "reply_text",
   settings,
-  function: generateText,
+  function: replyText,
 };
