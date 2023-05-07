08a743a8b3c497d79da621b11aa69006f981f5c5
Nicolas Simon <nsimon@pm.me>
2023-05-07 19:48:53 +0200

    Add Helicone for stats

--- telegram-gpt/src/bots/langchain/index.js
+++ telegram-gpt/src/bots/langchain/index.js
@@ -4,28 +4,39 @@ import ChatAgent from "./agents/chat";
 import SQLAgent from "./agents/sql";
 
 const getModel = () =>
-  new ChatOpenAI({
-    temperature: 0,
-    callbacks: [
-      // {
-      //   handleLLMStart(llm, prompts) {
-      //     console.log(`Prompts:`, prompts[0]);
-      //   },
-      // },
-      {
-        handleLLMEnd(out) {
-          console.log(
-            `LLM result (${out.llmOutput.tokenUsage.totalTokens}):\n${out.generations[0][0].text}`
-          );
+  new ChatOpenAI(
+    {
+      temperature: 0,
+      callbacks: [
+        // {
+        //   handleLLMStart(llm, prompts) {
+        //     console.log(`Prompts:`, prompts[0]);
+        //   },
+        // },
+        {
+          handleLLMEnd(out) {
+            console.log(
+              `LLM result (${out.llmOutput.tokenUsage.totalTokens}):\n${out.generations[0][0].text}`
+            );
+          },
+        },
+        {
+          handleLLMError(err) {
+            console.error("LLM error:", err.response.data.error.message);
+          },
+        },
+      ],
+    },
+    {
+      basePath: "https://oai.hconeai.com/v1",
+      baseOptions: {
+        headers: {
+          "Helicone-Auth": `Bearer ${process.env.HELICONE_API_KEY}`,
+          // TODO Helicone-User-Id
         },
       },
-      {
-        handleLLMError(err) {
-          console.error("LLM error:", err.response.data.error.message);
-        },
-      },
-    ],
-  });
+    }
+  );
 
 export const GPTChain = (input) => ChatAgent(getModel(), input);
 export const GPTSQL = (input) => SQLAgent(getModel(), input);
--- telegram-gpt/src/index.js
+++ telegram-gpt/src/index.js
@@ -3,12 +3,14 @@ import telegramChat from "./telegram";
 export default {
   async fetch(request, env) {
     process.env = { ...process.env, ...env };
+
     if (request.method === "POST") {
       const payload = await request.json();
       if ("message" in payload) {
         await telegramChat(payload.message);
       }
     }
+
     return new Response("Ok");
   },
 };
