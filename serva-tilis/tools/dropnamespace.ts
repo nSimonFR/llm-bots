28443ad56ecf962927e82fe8cca7f012b964b3e5
Nicolas Simon <nsimon@pm.me>
2023-05-09 12:11:04 +0200

    Add dropnamespace tool

---
+++ serva-tilis/tools/dropnamespace.ts
@@ -0,0 +1,24 @@
+import { PineconeClient } from "@pinecone-database/pinecone";
+
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const dropNamespace = async (namespace: string) => {
+  const client = new PineconeClient();
+  await client.init({
+    apiKey: process.env.PINECONE_API_KEY as string,
+    environment: process.env.PINECONE_ENV as string,
+  });
+  const index = client.Index(process.env.PINECONE_INDEX as string);
+
+  await index.delete1({ deleteAll: true, namespace });
+};
+
+const namespace = process.argv[2];
+if (namespace) {
+  dropNamespace(namespace).then(() => {
+    console.log("Done !");
+    process.exit(0);
+  });
+}
